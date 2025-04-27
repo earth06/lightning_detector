b1f52d811550d22878e7031045520e0f7c68071b
Takato Oonishi <nnnkjktkt119@gmail.com>
2025-04-27 21:29:52 +0900

    地理院タイルをセット

---
+++ src/tileserver/run_tileserver.py
@@ -0,0 +1,9 @@
+from src.tileserver.mbtileserver import create_app
+
+
+my_app = create_app(
+    mbtiles="src/master/chiriin_japan_z2_z9.mbtiles", cache_config={"CACHE_TYPE": "simple"}
+)
+
+if __name__ == "__main__":
+    my_app.run(debug=True, port=18888)
