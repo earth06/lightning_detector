04c43cb7e9c00afe1d779e7fb4493a3f75023503
Takato Oonishi <nnnkjktkt119@gmail.com>
2025-05-02 00:16:17 +0900

    loggerを属性に定義した親クラスを追加

---
+++ src/base.py
@@ -0,0 +1,16 @@
+import json
+from logging import config, getLogger
+
+
+class BaseClass:
+
+    def __init__(self):
+        with open("./log/log_config.json") as f:
+            log_conf = json.load(f)
+            config.dictConfig(log_conf)
+            self.logger = getLogger(__name__)
+
+
+if __name__=="__main__":
+    obj = BaseClass()
+    obj.logger.info("info")
