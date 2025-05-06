f11cf3db1717eb2c9e27f0517c8993f8fa58731d
Takato Oonishi <nnnkjktkt119@gmail.com>
2025-05-06 23:53:47 +0900

    webサーバーの起動のホスト、ポートをコマンドライン引数から指定できるようにした。

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,13 +1,15 @@
-from datetime import date, datetime, timedelta
-
 import dash
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import Input, Output, callback, dcc, html
+from dash import dcc, html
 
 from lightning_hyperbola import LightningHyperbola
 
 dash.register_page(__name__, path="/")
+
+HOST = "localhost"
+PORT = 18889
+
 
 def layout(**kwargs):
     # layout変数を定義しておくとマルチページ読み込みのときにapp.layoutに設定してくれるらしい
@@ -15,9 +17,8 @@ def layout(**kwargs):
     home_page = html.Div(
         children=[
             html.H2(children="雷検知デモ"),
-
             # 需給の対象エリア
-            dcc.Graph(id="map-graph", figure=update_demand_graph())
+            dcc.Graph(id="map-graph", figure=update_demand_graph()),
         ]
     )
     return home_page
@@ -34,15 +35,9 @@ def update_demand_graph():
     hyperbolas = lightning.get_hyperbola()
     fig = go.Figure()
     # 双曲線
-    for z_s,color in zip(hyperbolas["hyperbola"], colors, strict=False):
+    for z_s, color in zip(hyperbolas["hyperbola"], colors, strict=False):
         for z in z_s:
-            fig.add_trace(go.Scattermap(
-                mode="lines",
-                lon=z[0],
-                lat=z[1],
-                marker={"color":color}
-                )
-            )
+            fig.add_trace(go.Scattermap(mode="lines", lon=z[0], lat=z[1], marker={"color": color}))
 
     # 落雷地点
     fig.add_trace(
@@ -50,46 +45,38 @@ def update_demand_graph():
             mode="markers",
             lon=hyperbolas["lightning_point"][0],
             lat=hyperbolas["lightning_point"][1],
-            marker={"color":"black", "size":12,"symbol":["cross"]},
-
+            marker={"color": "black", "size": 12, "symbol": ["cross"]},
         )
     )
 
     # LLS地点
     fig.add_trace(
-    go.Scattermap(
-        mode="markers",
-        lon=lightning.station_master["lon"],
-        lat=lightning.station_master["lat"],
-        marker={"color":"darksalmon","size":14}
-    )
+        go.Scattermap(
+            mode="markers",
+            lon=lightning.station_master["lon"],
+            lat=lightning.station_master["lat"],
+            marker={"color": "darksalmon", "size": 14},
+        )
     )
 
     # 評定に使ったLLS
     fig.add_trace(
         go.Scattermap(
-        mode="markers",
-        lon=hyperbolas["use_station"]["lon"],
-        lat=hyperbolas["use_station"]["lat"],
-        marker={"color":"red", "size":14}
+            mode="markers",
+            lon=hyperbolas["use_station"]["lon"],
+            lat=hyperbolas["use_station"]["lat"],
+            marker={"color": "red", "size": 14},
         )
     )
     fig.update_layout(
         map_style="white-bg",
-            map_layers=[
-        {
-            "below": "traces",
-            "sourcetype": "raster",
-            "source": [
-                "http://100.66.1.13:18888/{z}/{x}/{y}.png"
-            ]
-        }
-      ],
-      map_zoom=6, map_center_lat = 35, map_center_lon=138,
-        margin={"r":0,"t":0,"l":0,"b":0},
-        height=800
-        )
+        map_layers=[
+            {"below": "traces", "sourcetype": "raster", "source": ["http://" + HOST + PORT + "/{z}/{x}/{y}.png"]}
+        ],
+        map_zoom=6,
+        map_center_lat=35,
+        map_center_lon=138,
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        height=800,
+    )
     return fig
-
-
-
--- src/tileserver/run_tileserver.py
+++ src/tileserver/run_tileserver.py
@@ -1,10 +1,12 @@
+import argparse
+
 from src.tileserver.mbtileserver import create_app
 
-
-my_app = create_app(
-    mbtiles="src/master/chiriin_japan_z2_z9.mbtiles", cache_config={"CACHE_TYPE": "simple"}
-)
+my_app = create_app(mbtiles="src/master/chiriin_japan_z2_z9.mbtiles", cache_config={"CACHE_TYPE": "simple"})
 
 if __name__ == "__main__":
-    #my_app.run(debug=True, port=18888)
-    my_app.run(host="100.66.1.13",debug=True, port=18888)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default="localhost", help="server's IP 100.66.1.13")
+    parser.add_argument("--port", default=18888, help="port number")
+    # my_app.run(debug=True, port=18888)
+    my_app.run(host="100.66.1.13", debug=True, port=18888)
